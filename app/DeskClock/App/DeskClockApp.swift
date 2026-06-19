37a2dd675cf320712b29ea6004ae01190a366ca8
Oscar PALISSOT <74594715+OscarPALISSOT@users.noreply.github.com>
2026-06-20 00:32:26 +0200

    init-app (#7)
    * init-app
    
    * update backend CI condition

---
+++ app/DeskClock/App/DeskClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeskClockApp.swift
+//  DeskClock
+//
+//  Created by Oscar PALISSOT on 18/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct DeskClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
